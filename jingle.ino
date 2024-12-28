d0746d1e7ebded42b441dcc0b02730e83193c06d
Nondescript_Potato <142705128+EMcConnell20@users.noreply.github.com>
2024-12-28 15:51:15 -0600

    Update jingle.ino

--- jingle.ino
+++ jingle.ino
@@ -3,6 +3,33 @@
 #include <WiFiClientSecure.h>
 #include <Adafruit_NeoPixel.h>
 
-// Network Consts //
-const char* SSID = "MCS_Guest";
-const char* SSID_PASSWORD = "";
+#define AUDIO_PIN D6
+
+const char* _SSID = "MCS_Guest";
+const char* _SSID_PASSWORD = "";
+const char* _FIREBASE_URL = "https://doorbell-338a5-default-rtdb.firebaseio.com/ALERT.json";
+
+// Setup //
+
+void setup() {
+    Serial.begin(115200);
+}
+
+// Loop //
+
+const unsigned long TIMER_INTERVAL = 1000;
+unsigned long apex_time = 0;
+bool buzz = false;
+
+void loop() {
+	unsigned long delta_time = millis();
+
+	if (delta_time - apex_time >= TIMER_INTERVAL) {
+		apex_time = delta_time;
+		buzz = !buzz;
+
+		if (buzz == true) {
+			tone(AUDIO_PIN, 3000, TIMER_INTERVAL);
+		}
+	}
+}
