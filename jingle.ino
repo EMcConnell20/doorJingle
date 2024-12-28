cd34eb509285cbca753f45138b5097b32a32560c
Nondescript_Potato <142705128+EMcConnell20@users.noreply.github.com>
2024-12-28 14:42:57 -0600

    Added Network Constants To jingle.ino

--- jingle.ino
+++ jingle.ino
@@ -1,1 +1,8 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include <Adafruit_NeoPixel.h>
 
+// Network Consts //
+const char* SSID = "MCS_Guest";
+const char* SSID_PASSWORD = "";
