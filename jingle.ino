a1a6e435b8470256532109c56983ffa4293d59c9
Joe Mc <joe.mcconnell10@gmail.com>
2024-12-28 18:00:08 -0600

    updates identified with ChatGPT

--- jingle.ino
+++ jingle.ino
@@ -12,7 +12,7 @@ enum Pitch {
   G,
   A,
   B,
-}
+};
 
 struct Note {
   Pitch pitch;
@@ -21,57 +21,42 @@ struct Note {
   byte degree; // 0 = Whole Note | 1 = Half Note | 2 = Quarter Note
 };
 
-unsigned int get_frequency(struct *Note note) {
+unsigned int get_frequency(const Note* note) {
   double hertz = 0.0;
 
-  switch(note.pitch) {
-    case (Pitch::C):
-      output = note.sharp ? 17.32 : 16.35;
-      break;
-    case (Pitch::D):
-      output = note.sharp ? 19.45 : 17.32;
-      break;
-    case (Pitch::E):
-      output = note.sharp ? 21.83 : 20.6;
-      break;
-    case (Pitch::F):
-      output = note.sharp ? 23.12 : 21.83;
-      break;
-    case (Pitch::G):
-      output = note.sharp ? 25.96 : 24.5;
-      break;
-    case (Pitch::A):
-      output = note.sharp ? 29.14 : 27.5;
-      break;
-    case (Pitch::B):
-      output = note.sharp ? 32.7 : 30.87;
-      break;
+  switch (note->pitch) {
+    case C: hertz = note->sharp ? 17.32 : 16.35; break;
+    case D: hertz = note->sharp ? 19.45 : 17.32; break;
+    case E: hertz = note->sharp ? 21.83 : 20.6; break;
+    case F: hertz = note->sharp ? 23.12 : 21.83; break;
+    case G: hertz = note->sharp ? 25.96 : 24.5; break;
+    case A: hertz = note->sharp ? 29.14 : 27.5; break;
+    case B: hertz = note->sharp ? 32.7 : 30.87; break;
   }
 
-  hertz *= pow(2.0, note.octive.toDouble());
-  return trunc(hertz);
+  hertz *= pow(2.0, static_cast<double>(note->octave));
+  return static_cast<unsigned int>(hertz);
 }
 
-const struct Note QUARTER_C3 = { Pitch::C, false, 3, 2 };
-const struct Note QUARTER_D3 = { Pitch::D, false, 3, 2 };
-const struct Note QUARTER_E3 = { Pitch::E, false, 3, 2 };
-const struct Note QUARTER_F3 = { Pitch::F, false, 3, 2 };
-const struct Note QUARTER_G3 = { Pitch::G, false, 3, 2 };
-const struct Note QUARTER_A4 = { Pitch::A, false, 4, 2 };
-const struct Note QUARTER_B4 = { Pitch::B, false, 4, 2 };
+const Note QUARTER_C3 = { C, false, 3, 2 };
+const Note QUARTER_D3 = { D, false, 3, 2 };
+const Note QUARTER_E3 = { E, false, 3, 2 };
+const Note QUARTER_F3 = { F, false, 3, 2 };
+const Note QUARTER_G3 = { G, false, 3, 2 };
+const Note QUARTER_A4 = { A, false, 4, 2 };
+const Note QUARTER_B4 = { B, false, 4, 2 };
 
 // Tempo & Timing //
 
 double tempo = 60.0;
 
-void play_note(struct *Note note) {
-  tone(AUDIO_PIN, get_frequency(note), note_durration(note.degree));
+unsigned int note_duration(byte degree) {
+  return static_cast<unsigned int>((60.0 / tempo * 1000.0) / pow(2.0, static_cast<double>(degree)));
 }
 
-unsigned int note_durration(byte degree) {
-  // Mins / BPM * 1000ms
-  // Auto Conversion Probably?
-  return unsigned int (60.0 / tempo * 1000.0 / pow(2.0, degree.toDouble()));
+void play_note(const Note* note) {
+  tone(AUDIO_PIN, get_frequency(note), note_duration(note->degree));
+  delay(note_duration(note->degree));
 }
 
 // Setup //
